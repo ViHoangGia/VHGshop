1a30a12fa6746c1fd6768eb63ef7ee4bfdc8c361
Hoàng Gia <vihoangia@gmail.com>
2022-12-06 09:58:10 +0700

    init

---
+++ ec/app/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+CITY_CHOICES = (
+    ('Vung Tau', 'Vũng Tàu'),
+    ('Ho Chi Minh', 'Hồ Chí Minh'),
+    ('Ha Noi', 'Hà Nội'),
+    ('Da Nang', 'Đà Nẵng')
+)
+
+CATEGORY_CHOICES = (
+    ('DT', 'Điện Thoại'),
+    ('TL', 'Máy tính Bảng'),
+    ('LT', 'Laptop'),
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    # composition = models.TextField(default='')
+    # prodapp = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')
+    def __str__(self):
+        return self.title
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    #city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    #zipcode = models.IntegerField()
+    city = models.CharField(choices = CITY_CHOICES, max_length=100, null=True)
+    def __str__(self):
+        return self.name
---
+++ ec/app/views.py
@@ -0,0 +1,67 @@
+from django.db.models import Count
+from django.shortcuts import render
+from django.views import View
+from . models import Customer, Product
+from . forms import CustomerRegistrationForm, CustomerProfileForm
+from django.contrib import messages
+
+# Create your views here.
+def home(request):
+    return render(request,"app/home.html")
+
+def about(request):
+    return render(request,"app/about.html")
+
+def contact(request):
+    return render(request,"app/contact.html")
+
+class CategoryView(View):
+    def get(self, request, val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title')
+        return render(request,"app/category.html", locals())
+
+class CategoryTitle(View):
+    def get(self, request, val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category=product[0].category).values('title')
+        return render(request,"app/category.html", locals())
+
+class ProductDetail(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request,"app/productdetail.html", locals())
+
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request,"app/customerregistration.html", locals())
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Bạn đã đăng ký thành công")
+        else:
+            messages.warning(request,"Thông tin không hợp lệ")
+        return render(request,"app/customerregistration.html", locals())
+
+class ProfileView(View):
+    def get(self, request):
+        form = CustomerProfileForm()
+        return render(request,"app/profile.html", locals())
+    def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data["name"]
+            address = form.cleaned_data["address"]
+            city = form.cleaned_data["city"]
+            mobile = form.cleaned_data["mobile"]
+
+            reg = Customer(user = user, name = name, address = address, city = city, mobile = mobile)
+            reg.save()
+            messages.success(request, "Chúc mừng! Hồ sơ đã lưu thành công")
+        else:
+            messages.warning(request, "Thông tin không hợp lệ")
+        return render(request,"app/profile.html", locals())
+
